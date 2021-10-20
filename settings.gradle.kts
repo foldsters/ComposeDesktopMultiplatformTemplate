de5c02b8eb5c5abdb325a35fd7bb93dd0e95c5fe
Foldster <foldsters@gmail.com>
2021-10-20 17:13:40 -0400

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        jcenter()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+    
+}
+rootProject.name = "ComposeDesktopMultiplatformTest"
+
+
+include(":android")
+include(":desktop")
+include(":common")
+
